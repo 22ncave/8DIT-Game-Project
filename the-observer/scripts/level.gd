3b1de36e869b338499dffd71f4e21329a6e13d72
22ncave <22ncave@wakatipu.school.nz>
2026-05-27 10:21:07 +1200

    Finnished map layout/Door placement

--- the-observer/scripts/level.gd
+++ the-observer/scripts/level.gd
@@ -10,8 +10,3 @@ func _on_level_spawn(position_tag: String):
 	var door_path = "Doors/Door_" + position_tag
 	var door = get_node(door_path) as Door
 	Globals.trigger_player_spawn(door.spawn.global_position, door.position_tag)
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
