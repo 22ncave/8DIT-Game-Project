1184bb8c52fc903f334f0b7b09b98b9a5f347782
22ncave <22ncave@wakatipu.school.nz>
2026-06-02 09:58:26 +1200

    made walls and basic monster code

---
+++ the-observer/scripts/monster.gd
@@ -0,0 +1,21 @@
+extends CharacterBody2D
+
+@onready var nav_agent = $NavigationAgent2D
+var speed = 150
+
+@export var entity_tag : String
+
+@export var player:CharacterBody2D
+
+func _physics_process(delta: float) -> void:
+	if not player:
+		return
+	
+	nav_agent.target_position = player.global_position
+	
+	var next_path_pos = nav_agent.get_next_path_position()
+	
+	var direction = (next_path_pos - global_position).normalized()
+	
+	velocity = direction * speed
+	move_and_slide()
