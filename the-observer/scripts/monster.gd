2b9189878e0a38d27a18c9200b06b8d070471972
22ncave <22ncave@wakatipu.school.nz>
2026-06-04 13:15:25 +1200

    added sight line for monster
    The monster now has a sight line and will only follow the player if the player has entered the sight line

--- the-observer/scripts/monster.gd
+++ the-observer/scripts/monster.gd
@@ -2,6 +2,7 @@ extends CharacterBody2D
 
 @onready var nav_agent = $NavigationAgent2D
 var speed = 150
+var sees_player = false
 
 @export var entity_tag : String
 
@@ -11,7 +12,8 @@ func _physics_process(delta: float) -> void:
 	if not player:
 		return
 	
-	nav_agent.target_position = player.global_position
+	if sees_player == true:
+		nav_agent.target_position = player.global_position
 	
 	var next_path_pos = nav_agent.get_next_path_position()
 	
@@ -21,6 +23,12 @@ func _physics_process(delta: float) -> void:
 	move_and_slide()
 
 
+
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
 		Globals.player_death()
+
+
+func _on_sight_line_body_entered(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		sees_player = true
